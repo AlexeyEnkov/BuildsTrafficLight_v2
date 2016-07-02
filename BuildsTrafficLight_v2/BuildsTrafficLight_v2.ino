4f83f5ae5a0c34c50777c64e6e57dd96fb26d4a4
Antonov Anton <war_anton@mail.ru>
2016-07-02 03:46:45 +0300

    initial

---
+++ BuildsTrafficLight_v2/BuildsTrafficLight_v2.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		BuildsTrafficLight_v2.ino
+ Created:	02.07.2016 3:45:53
+ Author:	Ant
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
