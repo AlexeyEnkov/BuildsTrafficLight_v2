774e9abe414b40ed4412ab114110459bd31b95d0
Alexey Enkov <alexey.enkov@gmail.com>
2016-07-28 12:19:13 +0300

    Split web page to html+css+js + load and save settings using js

--- esp8266 luascripts/server/server.lua
+++ esp8266 luascripts/server/server.lua
@@ -1,17 +1,20 @@
 local module=...
 local bodyHandler
 return function(c,request)
+    --print("--receive--\n"..request.."\n--end receive--\n")
     if bodyHandler then bodyHandler(c, request) bodyHandler=nil return end
     local _, _, method, path, vars = string.find(request, "([A-Z]+) (.+)?(.+) HTTP")
     if(method == nil) then _, _, method, path = string.find(request, "([A-Z]+) (.+) HTTP") end
-    local sender = require("sender")
-    if(path=="/favicon.ico") then require("send_file")(c,"favicon.png") return end
-    if(method == "POST" and path == "/settings") then
+    local sendfile = require("send_file")
+    if (path=="/favicon.ico") then sendfile(c,"favicon.png")
+    elseif (path=="/start_page.css") then sendfile(c,"start_page.css")
+    elseif (path=="/start_page.js") then sendfile(c,"start_page.js")
+    elseif (method == "POST" and path == "/settings") then
         bodyHandler = require("settings_handler")
         local _,_,body=string.find(request,"\r\n\r\n(.+)")
         if body then bodyHandler(c, request) bodyHandler=nil end
-        return
-    end
-    require("start_page")(c)
+    elseif (method == "GET" and path == "/settings") then
+        require("send_settings")(c)
+    else sendfile(c,"start_page.html") end
     if package.loaded[module] then package.loaded[module]=nil end
 end
