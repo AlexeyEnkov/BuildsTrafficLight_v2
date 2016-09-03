c913f1931d8985196177f699136217f2383b7c44
Antonov Anton <war_anton@mail.ru>
2016-09-03 16:45:24 +0300

    general refactoring for arduino code

--- BuildsTrafficLight_v2/BuildsTrafficLight_v2.ino
+++ BuildsTrafficLight_v2/BuildsTrafficLight_v2.ino
@@ -43,13 +43,14 @@ void setup() {
 	Timer1.attachInterrupt(routineProcess);
 	Timer1.start();
 
-	SystemConfig.initFromEEPROM();// init system settings stored in eeprom
-	SystemConfig.updateDebugMode(false); //debug off temporary
+	//SystemConfig.initFromEEPROM();// init system settings stored in eeprom
+//	SystemConfig.updateDebugMode(false); //debug off temporary
 
-	SoundManager.playInitSound();
-	
+	//SoundManager.playInitSound();
+	/*
 	Serial.println(WifiUtils.reset());
-	Serial.println(WifiUtils.testWifi(true));
+	Serial.println(WifiUtils.testWifi(true));*/
+	Serial1.setTimeout(500);
 }
 
 boolean isSetupMode = false;
@@ -68,49 +69,119 @@ void routineProcess()
 	counter++;
 }
 
-void loop() {
-	if (SystemConfig.isDebugMode())
-	{
-		SystemUtils.printFreeMemory();
-	}
-
-	// process current state of system (main process)
-	system.process();
-
-	long del = system.getDelayAfterProcess();
-	boolean needReadConf = true;
-	while (del > 0)
-	{
-		if (needReadConf)
-		{
-			WifiUtils.runScript(F("get_conf.lua"));
-			String resp = WifiUtils.readResponce();
-			if (resp.length() > 0 && resp.startsWith(F("CFG:")))
-			{
-				resp = resp.substring(4);
-				//Serial.println(resp);
-				SystemConfigHelper.handleCfg(resp);
-			}
-			needReadConf = false;
-		}
-		del -= 10;
-		delay(10);
-	}
-}
+//// init
+//InitSystemLightStrategy initSystemL;  //0
+//// problems
+//BuildServerErrorLightStrategy buildServerErrorL; //1
+//BuildServerRequestErrorLightStrategy buildServerRequestErrorL; //2
+//WiFiConnectionErrorLightStrategy wifiErrorL; //3
+//// builds
+//BuildsFailedLightStrategy buildsFailedL; //4
+//BuildsSuccessLightStrategy buildsSucessedL; //5
+//BuildsFailedAndRunningLightStrategy buildsRunningL; //6
 
 /*
-//TODO pseudo code for feature
-get cfg from module
+	commands map
+	L - light (L<NUMBER OF LIGHT STRATEGY>)
+	L0,L1,L2,L3,L4,L5,L6
+	S - sound (S<NUMBER OF SOUND>)
+	S0,S1,S2
+	C - config
 
-get status OK, C_ERR, R_ERR, OTHER
-if OK - get SUCC,RUN,FAIL
-
-C_ERR - WIFI? WifiError or BuildServerError
-R_ERR - BuildServerErrorLightStrategy
-
-
-SUCC - BuildSeccuessLightStr - good sound if need
-FAIL - BuildFailedLightStr - fails sound
-RUN - BuildFailedAndRunningStr
-
+	Command starts with $ and ends with $.First char is type of command ($<CMD><DATA>$)
 */
+
+void loop() {
+	//if (SystemConfig.isDebugMode())
+	//{
+	//	SystemUtils.printFreeMemory();
+	//}
+
+	//// process current state of system (main process)
+	//system.process();
+
+	//long del = system.getDelayAfterProcess();
+	//boolean needReadConf = true;
+	//while (del > 0)
+	//{
+	//	if (needReadConf)
+	//	{
+	//		WifiUtils.runScript(F("get_conf.lua"));
+	//		String resp = WifiUtils.readResponce();
+	//		if (resp.length() > 0 && resp.startsWith(F("CFG:")))
+	//		{
+	//			resp = resp.substring(4);
+	//			//Serial.println(resp);
+	//			SystemConfigHelper.handleCfg(resp);
+	//		}
+	//		needReadConf = false;
+	//	}
+	//	del -= 10;
+	//	delay(10);
+	//}
+	String input;
+	
+	if (Serial1.available())
+	{
+		input = Serial1.readString();
+		input.trim();
+	}
+
+	if (validateInput(input))
+	{
+		input = input.substring(1, (input.length() - 1));
+		String data = input.substring(1);
+		Serial.println(data);
+		switch (input.charAt(0))
+		{
+		case 'L':
+			Serial.println("LIGHT");
+			break;
+		case 'S':
+			Serial.println("SOUND");
+			break;
+		case 'C':
+			Serial.println("CONFIG");
+			break;
+		default:
+			break;
+		}
+	}
+
+
+	
+}
+
+
+boolean validateInput(String input)
+{
+	if (input.length() < 4)
+	{
+		return false;
+	}
+	if (!input.startsWith("$"))
+	{
+		return false;
+	}
+	if (input.indexOf("$", 1) != (input.length() - 1))
+	{
+		return false;
+	}
+
+	return true;
+}
+
+void setLightStrategy(String data)
+{
+
+}
+
+void updateSettings(String data)
+{
+
+}
+
+void playSound(String data)
+{
+
+}
